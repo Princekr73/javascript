06208b7e5345b3eead9ad106cd88c2f2ef682780
Prince Kumar <133627107+Princekr73@users.noreply.github.com>
2024-12-25 16:59:36 +0000

    datatypes summary added

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -30,3 +30,30 @@ const myFunction = function(){
     console.log("Hello world");
 }
 console.log(typeof outsideTemp);
+
+
+
+//  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "princerajkumar"
+let anothername = myYoutubename
+anothername = "newchannel"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "prince@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
