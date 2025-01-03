249cc0ee9b0bdc6dcf28dddda9b47d7be146fc82
Prince Kumar <133627107+Princekr73@users.noreply.github.com>
2025-01-03 16:49:36 +0000

    iife session

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named iife
+    console.log(`DB CONNECTED`);
+    
+})();
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+})('prince')
